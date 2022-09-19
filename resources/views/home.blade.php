a3fa4aaf1c52828d735af61dd3c8b44eb4374493
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-19 13:50:09 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('main-content')
+    <div class="container mt-5">
+        <h1>Benvenuto!</h1>
+    </div>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="{{ url('/') }}">Home</a>
+        </div>
+      </nav>
+</header>
